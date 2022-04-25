6d676f1b3c593e5d94d8d28d3e89fe2dfba4fb49
戴凯杰 <xpress@danwi.me>
2022-04-25 18:07:12 +0800

    临时提交

--- src/main/kotlin/me/danwi/sqlex/example/Main.kt
+++ src/main/kotlin/me/danwi/sqlex/example/Main.kt
@@ -1,20 +1,10 @@
 package me.danwi.sqlex.example
 
-import me.danwi.sqlex.core.DataSourceManager
+import me.danwi.sqlex.core.DaoFactory
 import me.danwi.sqlex.example.dao.Repository
-import me.danwi.sqlex.example.dao.RoleDao
-import me.danwi.sqlex.example.dao.UserDao
+
 
 fun main() {
-    val dataSourceManager = DataSourceManager()
-    val dataSource = dataSourceManager.getInstance(Repository::class.java)
-    val userDao = dataSource.getInstance(UserDao::class.java)
-    val roleDao = dataSource.getInstance(RoleDao::class.java)
-
-    userDao.findAll(10, "123", null).firstOrNull()
-    userDao.findAll(10, "123", listOf<String>()).firstOrNull()
-    userDao.findAllByRole("123").firstOrNull()
-    userDao.findCountsByRole().firstOrNull()
-
-    roleDao.findAll("管").firstOrNull()
+    val factory = DaoFactory("jdbc:mysql://localhost:3306/sqlex", "root", "1234qwer", Repository::class.java)
+    factory.check()
 }
