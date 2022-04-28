3b4b23114733692c480e4c758e001c4097dd80bc
戴凯杰 <xpress@danwi.me>
2022-04-28 18:26:57 +0800

    适配0.3.0

--- src/main/kotlin/me/danwi/sqlex/example/Main.kt
+++ src/main/kotlin/me/danwi/sqlex/example/Main.kt
@@ -1,10 +1,24 @@
 package me.danwi.sqlex.example
 
 import me.danwi.sqlex.core.DaoFactory
+import me.danwi.sqlex.core.exception.SqlExCheckException
 import me.danwi.sqlex.example.dao.Repository
 
 
 fun main() {
-    val factory = DaoFactory("jdbc:mysql://localhost:3306/sqlex", "root", "1234qwer", Repository::class.java)
-    factory.check()
+    val factory = DaoFactory(
+        "jdbc:mysql://localhost:3306/sqlex?tinyInt1isBit=false&yearIsDateType=true",
+        "root",
+        "1234qwer",
+        Repository::class.java
+    )
+    try {
+        factory.check()
+    } catch (ex: SqlExCheckException) {
+        ex.tables.forEach { t ->
+            t.columns.forEach { f ->
+                println("数据库异常: ${t.name} - ${f.name} - ${f.typeName}")
+            }
+        }
+    }
 }
