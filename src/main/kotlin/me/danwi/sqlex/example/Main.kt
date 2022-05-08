19a52a519fdef8250c81d4126f64ad2bf04edff5
戴凯杰 <xpress@danwi.me>
2022-05-08 19:31:25 +0800

    升级sqlex至0.7.1

--- src/main/kotlin/me/danwi/sqlex/example/Main.kt
+++ src/main/kotlin/me/danwi/sqlex/example/Main.kt
@@ -1,20 +1,28 @@
 package me.danwi.sqlex.example
 
-import me.danwi.sqlex.core.DataSourceManager
+import me.danwi.sqlex.core.DaoFactory
 import me.danwi.sqlex.example.dao.Repository
 import me.danwi.sqlex.example.dao.RoleDao
 import me.danwi.sqlex.example.dao.UserDao
 
 fun main() {
-    val dataSourceManager = DataSourceManager()
-    val dataSource = dataSourceManager.getInstance(Repository::class.java)
-    val userDao = dataSource.getInstance(UserDao::class.java)
-    val roleDao = dataSource.getInstance(RoleDao::class.java)
+    val factory = DaoFactory(
+        "jdbc:mysql://localhost:3306/sqlex",
+        "root",
+        "1234qwer",
+        Repository::class.java
+    )
+    // 执行迁移任务
+    factory.migrate()
+    // 比对差异
+    factory.check()
 
+    val userDao = factory.getInstance(UserDao::class.java)
     userDao.findAll(10, "123", null).firstOrNull()
     userDao.findAll(10, "123", listOf<String>()).firstOrNull()
     userDao.findAllByRole("123").firstOrNull()
     userDao.findCountsByRole().firstOrNull()
-
+    
+    val roleDao = factory.getInstance(RoleDao::class.java)
     roleDao.findAll("管").firstOrNull()
 }
